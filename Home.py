9327b3a4881a8581730abd23741e0e22150412e4
dbergman282 <david.bergman@business.uconn.edu>
2025-05-30 06:51:22 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,6 +6,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed"
 )
+st.image("MAESTRO_logo.png", width=200)
 st.title("📘 AI-Powered Case Study Assistant for UConn School of Business Faculty Retreat")
 st.markdown("Login, and work with this AI to create a new case for one of your classes next year based on a news story. Note that your convesation will be recorded and continuing with this demo confirms that you are aware of this.")
 
